9efc101d7d31292c4b5453d2affac528406e46c0
SergeyMashkevich <SergeyMashkevich@users.noreply.github.com>
2022-01-27 13:06:50 +0100

    .

---
+++ js.js
@@ -0,0 +1,12 @@
+//age calculation.
+function getAge(d1, d2) {
+    d2 = d2 || new Date();
+    var diff = d2.getTime() - d1.getTime();
+    return Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25));
+}
+
+var age = getAge(new Date(2004, 04, 07));
+
+
+document.getElementById("age").innerHTML = age;
+//
